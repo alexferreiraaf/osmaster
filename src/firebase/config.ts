94ff9986593a6e7d5f548192d86b9b48a421368e
Alex Ferreira <alexferreira.AF411@gmail.com>
2026-01-28 17:46:39 +0000

    const firebaseConfig = {   apiKey: "AIzaSyCfdAQImbf5WaVS_PO8JNtpIqv9P_l6

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,10 +4,10 @@
 // https://console.firebase.google.com/
 
 export const firebaseConfig = {
-    apiKey: "your-api-key",
-    authDomain: "your-auth-domain",
-    projectId: "your-project-id",
-    storageBucket: "your-storage-bucket",
-    messagingSenderId: "your-messaging-sender-id",
-    appId: "your-app-id"
+    apiKey: "AIzaSyCfdAQImbf5WaVS_PO8JNtpIqv9P_l6DB4",
+    authDomain: "studio-1463624237-478a3.firebaseapp.com",
+    projectId: "studio-1463624237-478a3",
+    storageBucket: "studio-1463624237-478a3.appspot.com",
+    messagingSenderId: "550271956121",
+    appId: "1:550271956121:web:cb02ec5c9264da37933408"
 };
